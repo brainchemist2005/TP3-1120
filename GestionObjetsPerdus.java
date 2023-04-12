c259fd825efd71e615c1e84d4e96084ff463044c
brainchemist2005 <zackariaebouargan@gmail.com>
2023-04-12 19:40:34 -0400

    Add files via upload

--- GestionObjetsPerdus.java
+++ GestionObjetsPerdus.java
@@ -1,7 +1,57 @@
 
 public class GestionObjetsPerdus {
 	final static String ERREUR_MENU = "ERREUR, vous devez entrer un choix entre 1 et 7! RECOMMENCEZ...";
-}
+	
+	public static void consignerObjet() {
+		String choix,type;
+		
+		do {
+		
+		System.out.println("------------------------\r\n"
+				+ "CONSIGNER UN OBJET PERDU\r\n"
+				+ "------------------------\r\n"
+				+ "\r\n"
+				+ "CATEGORIES :\r\n"
+				+ "1. bijou\r\n"
+				+ "2. vetement\r\n"
+				+ "3. argent / portefeuille\r\n"
+				+ "4. cle(s)\r\n"
+				+ "5. autre");
+		
+		System.out.print("Entrez votre choix : ");
+		choix = Clavier.lireString();
+
+            switch (choix) {
+                case "1":
+                	type = "bijou" ;
+                    break;
+
+                case "2":
+                    type = "vetement";
+                    break;
+
+                case "3":
+                	type = "argent / portefeuille";
+                    break;
+
+                case "4":
+                	type = "cle(s)";
+                    break;
+                    
+                case "5":
+                	type = "autre";
+                    break;
+                    
+                default:
+                    System.out.println(ERREUR_MENU);
+            }
+		
+		}while(choix.equals("1") || choix.equals("2") || choix.equals("3") || choix.equals("4") || choix.equals("5"));
+		
+		System.out.println("Date de consignation : ");
+		
+	}
+	
 	public static void menu() {
 		System.out.println("=========================\r\n"
 				+ "GESTION DES OBJETS PERDUS\r\n"
@@ -22,7 +72,7 @@ public class GestionObjetsPerdus {
 	
 	public static void main(String[] args) {
         String choix;
-        ObjetPerdu[] objetsPerdus;
+        ObjetPerdu[] objetsPerdus = TP3Utils.recupererDonnees();
 
         do {
             menu();
@@ -32,7 +82,7 @@ public class GestionObjetsPerdus {
 
             switch (choix) {
                 case "1":
-                    
+                	consignerObjet();
                     break;
 
                 case "2":
@@ -53,6 +103,7 @@ public class GestionObjetsPerdus {
                 	
                     break;
                 case "7":
+                	TP3Utils.sauvegarder(objetsPerdus);
                 	System.out.print("AUREVOIR !");
                     break;
                     
@@ -61,5 +112,6 @@ public class GestionObjetsPerdus {
                     System.out.println(ERREUR_MENU);
             }
 
-            } while (!choix.equals("7")); /// this DOESN'T WORK s.equals();
-}
+            } while (!choix.equals("7"));
+	}
+	}
